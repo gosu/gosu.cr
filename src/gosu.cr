7528024d6aaaf729d006a4086a584fdbcf9cb676
Cyberarm <matthewlikesrobots@gmail.com>
2019-09-14 11:30:10 -0500

    Color constructors

--- src/gosu.cr
+++ src/gosu.cr
@@ -20,6 +20,7 @@ module Gosu
     fun fps = Gosu_fps : UInt32
 
     fun button_down = Gosu_button_down(id : UInt32) : Bool
+    fun milliseconds = Gosu_milliseconds : UInt64
     fun draw_rect = Gosu_draw_rect(x : Float64, y : Float64, width : Float64, height : Float64,
                                    color : UInt32, z : Float64, mode : UInt32)
   end
@@ -34,8 +35,12 @@ module Gosu
     GosuC.button_down(id)
   end
 
+  def self.milliseconds : UInt64
+    GosuC.milliseconds
+  end
+
   def self.draw_rect(x : Float64|Int32, y : Float64|Int32, width : Float64|Int32, height : Float64|Int32,
-                     color : Gosu::Color|UInt32 = 0xff_ffffff, z : Float64|Int32 = 0, mode : UInt32 = 0)
+                     color : Gosu::Color|Int64|UInt32 = 0xff_ffffff, z : Float64|Int32 = 0, mode : UInt32 = 0)
     GosuC.draw_rect(x.to_f64, y.to_f64, width.to_f64, height.to_f64,
                     color.is_a?(Gosu::Color) ? color.gl : color, z.to_f64, mode)
   end
--- src/gosu/color.cr
+++ src/gosu/color.cr
@@ -1,10 +1,60 @@
 module Gosu
   @[Link("gosu")]
   lib ColorC
+    fun create_color = Gosu_Color_create(color : UInt32) : UInt32
+    fun create_color_argb = Gosu_Color_create_argb(alpha : UInt8, red : UInt8, green : UInt8, blue : UInt8) : UInt32
+    fun create_color_from_hsv = Gosu_Color_create_from_hsv(hue : Float64, saturation : Float64, value : Float64) : UInt32
+    fun create_color_from_ahsv = Gosu_Color_create_from_ahsv(alpha : UInt8, hue : Float64, saturation : Float64, value : Float64) : UInt32
+    fun color_gl = Gosu_Color_gl(color : UInt32) : UInt32
   end
 
   class Color
+    def self.argb(color : UInt32|Int64) : Gosu::Color
+      return Gosu::Color.new(color)
+    end
 
-    RED = 0xff_ff0000.to_ui32
+    def self.argb(alpha : UInt8|Int64, red : UInt8|Int64, green : UInt8|Int64, blue : UInt8|Int64) : Gosu::Color
+      return Gosu::Color.new(alpha.to_u8, red.to_u8, green.to_u8, blue.to_u8)
+    end
+
+    def self.rgba(red : UInt8|Int64, green : UInt8|Int64, blue : UInt8|Int64, alpha : UInt8|Int64) : Gosu::Color
+      Gosu::Color.argb(alpha.to_u8, red.to_u8, green.to_u8, blue.to_u8)
+    end
+
+    def self.rgb(red : UInt8|Int64, green : UInt8|Int64, blue : UInt8|Int64) : Gosu::Color
+      Gosu::Color.argb(255.to_u8, red.to_u8, green.to_u8, blue.to_u8)
+    end
+
+    def self.hsv(hue : Float64, saturation : Float64, value : Float64) : Gosu::Color
+      Gosu::Color.new(ColorC.create_color_from_hsv(hue, saturation, value))
+    end
+
+    def self.ahsv(alpha : UInt8, hue : Float64, saturation : Float64, value : Float64) : Gosu::Color
+      Gosu::Color.new(ColorC.create_color_from_ahsv(alpha.to_u8, hue, saturation, value))
+    end
+
+    def initialize(color : UInt32|Int64)
+      @color = ColorC.create_color(color)
+    end
+
+    def initialize(alpha : UInt8, red : UInt8, green : UInt8, blue : UInt8)
+      @color = ColorC.create_color_argb(alpha, red, green, blue)
+    end
+
+    def gl : UInt32
+      ColorC.color_gl(@color)
+    end
+
+    NONE    = Gosu::Color.argb(0x00_000000)
+    BLACK   = Gosu::Color.argb(0xff_000000)
+    GRAY    = Gosu::Color.argb(0xff_808080)
+    WHITE   = Gosu::Color.argb(0xff_ffffff)
+    AQUA    = Gosu::Color.argb(0xff_00ffff)
+    RED     = Gosu::Color.argb(0xff_ff0000)
+    GREEN   = Gosu::Color.argb(0xff_00ff00)
+    BLUE    = Gosu::Color.argb(0xff_0000ff)
+    YELLOW  = Gosu::Color.argb(0xff_ffff00)
+    FUCHSIA = Gosu::Color.argb(0xff_ff00ff)
+    CYAN    = Gosu::Color.argb(0xff_00ffff)
   end
 end
