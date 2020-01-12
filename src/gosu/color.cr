a306f57732b72d7a642f9db74f93a25bb5aa602a
Cyberarm <matthewlikesrobots@gmail.com>
2020-01-12 09:54:07 -0600

    Added basic comments for Color

--- src/gosu/color.cr
+++ src/gosu/color.cr
@@ -29,11 +29,14 @@ module Gosu
     fun color_gl = Gosu_Color_gl(color : UInt32) : UInt32
   end
 
+  # Represents an ARGB color value with 8 bits for each channel. Colors can be used interchangeably with integer literals of the form 0xAARRGGBB in all Gosu APIs.
   class Color
+    # Converts Int32 to UInt8 safely, by clamping value to range [0; 255] then converting to UInt8.
     def self.safe_to_u8(value : Int32 | UInt8) : UInt8
       value.clamp(0, 255).to_u8
     end
 
+    # Creates color from 0xAARRGGBB integer literal.
     def self.argb(color : UInt32) : Gosu::Color
       return Gosu::Color.new(color)
     end
@@ -50,78 +53,97 @@ module Gosu
       Gosu::Color.argb(safe_to_u8(255), safe_to_u8(red), safe_to_u8(green), safe_to_u8(blue))
     end
 
+    # Converts an HSV triplet to an opaque color.
     def self.from_hsv(hue : Float64, saturation : Float64, value : Float64) : Gosu::Color
       Gosu::Color.new(ColorC.create_color_from_hsv(hue % 360.0, saturation.clamp(0.0, 1.0), value.clamp(0.0, 1.0)))
     end
 
+    # Converts an HSV triplet to a color with the alpha channel set to a given value.
     def self.from_ahsv(alpha : UInt8 | Int32, hue : Float64, saturation : Float64, value : Float64) : Gosu::Color
       Gosu::Color.new(ColorC.create_color_from_ahsv(safe_to_u8(alpha), hue % 360.0, saturation.clamp(0.0, 1.0), value.clamp(0.0, 1.0)))
     end
 
+    # Creates color from 0xAARRGGBB integer literal.
     def initialize(color : UInt32)
       @color = ColorC.create_color(color)
     end
 
+    # Creates color from four unsigned 8-bit integers.
     def initialize(alpha : UInt8 | Int32, red : UInt8 | Int32, green : UInt8 | Int32, blue : UInt8 | Int32)
       @color = ColorC.create_color_argb(Color.safe_to_u8(alpha), Color.safe_to_u8(red), Color.safe_to_u8(green), Color.safe_to_u8(blue))
     end
 
+    # Returns the color's alpha channel.
     def alpha : UInt8
       ColorC.alpha(@color)
     end
 
+    # Set the color's alpha channel.
     def alpha=(value : UInt8 | Int32) : UInt32
       @color = ColorC.set_alpha(@color, Color.safe_to_u8(value))
     end
 
+    # Returns the color's red channel.
     def red : UInt8
       ColorC.red(@color)
     end
 
+    # Sets the color's red channel.
     def red=(value : UInt8 | Int32) : UInt32
       @color = ColorC.set_red(@color, Color.safe_to_u8(value))
     end
 
+    # Returns the color's green channel.
     def green : UInt8
       ColorC.green(@color)
     end
 
+    # Sets the color's green channel.
     def green=(value : UInt8 | Int32) : UInt32
       @color = ColorC.set_green(@color, Color.safe_to_u8(value))
     end
 
+    # Returns the color's blue channel.
     def blue : UInt8
       ColorC.blue(@color)
     end
 
+    # Sets the color's green channel.
     def blue=(value : UInt8 | Int32) : UInt32
       @color = ColorC.set_blue(@color, Color.safe_to_u8(value))
     end
 
+    # Returns the color's hue.
     def hue : Float64
       ColorC.hue(@color)
     end
 
+    # Sets the color's hue.
     def hue=(value : Float64 | Int32) : UInt32
       @color = ColorC.set_hue(value.to_f64 % 360.0)
     end
 
+    # Returns the color's saturation.
     def saturation : Float64
       ColorC.saturation(@color)
     end
 
+    # Sets the color's saturation.
     def saturation=(value : Float64 | Int32) : UInt32
       @color = ColorC.set_saturation(value.to_f64.clamp(0.0, 1.0))
     end
 
+    # Returns the color's value (lightness).
     def value : Float64
       ColorC.value(@color)
     end
 
+    # Sets the color's value.
     def value=(value : Float64 | Int32) : UInt32
       @color = ColorC.set_value(value.to_f64.clamp(0.0, 1.0))
     end
 
+    # Returns a 32-bit representation of the color suitable for use with OpenGL calls. This color is stored in a fixed order in memory and its integer value may vary depending on your system's byte order.
     def gl : UInt32
       ColorC.color_gl(@color)
     end
